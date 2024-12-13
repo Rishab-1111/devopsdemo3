3489691102f8cb0489d44a654b8156722e009f3d
Rishab Raina <rishabraina2011@gmail.com>
2024-12-13 18:23:37 +0530

    commit msg

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author LAPZONE
+ *
+ */
+module devopsdemo3 {
+}
---
+++ src/set/v1.java
@@ -0,0 +1,10 @@
+package set;
+
+public class v1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
